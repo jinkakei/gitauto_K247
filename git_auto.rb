c9cb7f0c782b9ca9badea8757e34cedf76daeab0
Kei Nishina <k247loveke@gmail.com>
2015-09-18 19:22:40 +0859

    2015-09-18 19:22 End of Day

--- git_auto.rb
+++ git_auto.rb
@@ -23,10 +23,12 @@ require "open3"
   # under construction
   def popen3_wrap( cmd )
     o_str = Array(1)
+    w = 0
     Open3.popen3( cmd ) do | stdin, stdout, stderr, wait_thread|
       stdout.each_with_index do |line,n|
         o_str[n] = line
       end
+      w = wait_thread
     end
     ret = {"key_meaning"=>"i: stdin, o: stdout, e: stderr, w: wait_thread"}
       ret["o"] = o_str
@@ -51,26 +53,30 @@ require "open3"
   puts "Check: git status"
   puts "git status (stdout) line num: #{gstat["o"].length}"
 
-#=begin
-  is_mod_file = "Changes not staged for commit"
+  #is_mod_file = "#Changes not staged for commit\n"
+  # cannot distinct by bellow command
+    #p gstat["o"].include?(is_mod_file)
   m_kword = "#\tmodified:   "
   gstat["o"].each do | line |
     if line.include?(m_kword)
       m_file = line.split(m_kword)[1].chomp
-      puts "file: #{m_file} is modified."
       gdiff = popen3_wrap("git diff #{m_file}")
+      p gdiff
+      # ! ToDo: distinct m_file is already git add or not @ 2015-09-18
+=begin
+      puts "file: #{m_file} is modified."
       gdiff["o"].each do | line |  puts "    #{line}"  end
       answer = get_y_or_n( "\ngit add #{m_file}? (answer y/n): " )
       if answer == "y"
         puts "git add #{m_file}"
         gadd = popen3_wrap("git add #{m_file}")
-        gadd["o"].each do | line |  puts "    #{line}"  end
+      #  gadd["o"].each do | line |  puts "    #{line}"  end
       else
         puts "#{m_file} was modified but not added"
       end
+=end
     end
   end
-#=end
 #  gstat["o"].each do | line | p line end
 #  untracked_kword = "Untracked files"
 
