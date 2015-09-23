779f7b268d5a590643fe3afae7e2c1d622501381
Kei Nishina <k247loveke@gmail.com>
2015-09-23 09:00:23 +0859

    update

--- git_k247.rb
+++ git_k247.rb
@@ -19,7 +19,8 @@ module Git_K247
 #  git_add_interactive
 #  git_commit_interactive
 #    exec_command
-
+#  get_gitdirs
+#  git_gdir_each
 
 # common part
   # 2015-09-18: create
@@ -60,7 +61,6 @@ module Git_K247
 
 
 
-
 def git_push_interactive( arg = nil )
 
   gstat = popen3_wrap("git remote show origin")
@@ -72,7 +72,7 @@ def git_push_interactive( arg = nil )
     answer = get_y_or_n( "git push? (answer y/n): " )
     if answer == "y"
       ret = popen3_wrap("git push -v origin master:master")
-      show_stdoe( ret )
+      show_stdoe( ret ); print "\n\n\n"
     else # if answer == "y"
       puts "  you choose not to git push"; print "\n\n\n"
     end # if answer == "y"
@@ -88,7 +88,6 @@ end # def git_push_interactive( arg = nil )
 
 
 
-
 def git_pull_interactive( arg = nil )
 
   gstat = popen3_wrap("git remote show origin")
@@ -100,7 +99,7 @@ def git_pull_interactive( arg = nil )
     answer = get_y_or_n( "git pull? (answer y/n): " )
     if answer == "y"
       ret = popen3_wrap("git pull")
-      show_stdoe( ret )
+      show_stdoe( ret ); print "\n\n\n"
     else # if answer == "y"
       puts "  you choose not to git push"; print "\n\n\n"
     end # if answer == "y"
@@ -130,6 +129,55 @@ end # def git_commit_interactive
     puts "[end]#{cmd}: #{ret}"
     print "\n\n"
   end
+
+
+def get_gitdirs 
+  current_dir = Dir.pwd
+  home = ENV['HOME']
+
+  gdfile = "#{home}/git_dir_k247.txt"
+  gitdirs = Array.new(1)
+  open( gdfile, "r").each_with_index{ | line, n |
+    gitdirs[n] = line.chomp
+  }
+  return gitdirs
+end # def get_gitdirs 
+
+
+def git_gdir_each( arg=nil )
+  if arg == "pull" || arg == "push"
+    gact = arg
+  else
+    gact = "pull"
+    puts "Wrong argument #{arg}, set git action pull"
+  end
+
+  gitdirs = get_gitdirs
+    gret = Array( gitdirs.length )
+  gitdirs.each_with_index do | gdir,n |
+    ret = Dir::chdir( gdir )
+    puts "Current dir: #{Dir::getwd}"
+    unless File::exist?(".git")
+      puts "!Caution! This directory is not registered on git!"
+      gret[n] = -1
+    else
+      if gact == "pull"
+        gret[n] = git_pull_interactive
+      else # "push"
+        git_commit_interactive
+        gret[n] = git_push_interactive
+      end # if gact == "pull"
+    end
+  end # gitdirs.each_with_index do | gdir,n |
+  
+  print "\n\n\n"; puts "Results (0: true, -1: false)"
+  gitdirs.each_with_index do | gdir,n |
+    puts "  #{gdir} (#{gret[n]})"
+  end
+  print "\n"
+
+  return 0
+end # def git_gdir_each( arg=nil )
 
 
 end # module Git_K247
--- test.rb
+++ test.rb
@@ -1,59 +1,15 @@
+#!/usr/bin/ruby
 require "git_k247"
 include Git_K247
 
-#Git_K247::git_add_interactive
 
-  def get_gitdirs 
-    current_dir = Dir.pwd
-    home = ENV['HOME']
-  
-    gdfile = "#{home}/git_dir_k247.txt"
-    gitdirs = Array.new(1)
-    puts "Git Directories"
-    open( gdfile, "r").each_with_index{ | line, n |
-      gitdirs[n] = line.chomp
-      #puts "  #{n}: #{gitdirs[n]}"
-    }
-    return gitdirs
-  end # def get_gitdirs 
+#ret = git_gdir_each( "pull")
+#ret = git_gdir_each( "push")
+
+#ret = exec_command("exit") # cannot exit
 
 
-def git_gdir_each( arg=nil )
-  if arg == "pull" || arg == "push"
-    gact = arg
-  else
-    gact = "pull"
-    puts "Set git action pull"
-  end
 
-  gitdirs = get_gitdirs
-    gret = Array( gitdirs.length )
-  gitdirs.each_with_index do | gdir,n |
-    ret = Dir::chdir( gdir )
-    puts "Current dir: #{Dir::getwd}"
-    unless File::exist?(".git")
-      puts "!Caution! This directory is not registered on git!"
-      gret[n] = -1
-    else
-      if gact == "pull"
-        gret[n] = git_pull_interactive
-      else # "push"
-        git_commit_interactive
-        gret[n] = git_push_interactive
-      end # if gact == "pull"
-    end
-  end
-  
-  print "\n\n\n"; puts "Results (0: true, -1: false)"
-  gitdirs.each_with_index do | gdir,n |
-    puts "  #{gdir} (#{gret[n]})"
-  end
-  print "\n"
-  
-  return 0
-end # def git_gdir_each( arg=nil )
+puts "IN: ruby script"
 
-
-ret = git_gdir_each
-
-
+exit -1
